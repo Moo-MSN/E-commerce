65d2da25650d46131191594383e181c3b63942bb
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-04-20 16:56:45 +0700

    Add actions for search product

--- src/stores/user/product.js
+++ src/stores/user/product.js
@@ -11,7 +11,20 @@ export const useProductStore = defineStore("product", {
         atatus: "open",
         price: 100,
       },
+      {
+        // เพิ่มจำนวน product
+        name: "Moo Test",
+        imageURL: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+        quantity: 10,
+        about: "testt",
+        atatus: "open",
+        price: 120,
+      },
     ],
-    
   }),
+  actions: { // เพิ่ม action searchText เพื่อใช้ในการค้นหา product
+    filterProducts (searchText) {
+      return this.list.filter(Product => Product.name.includes(searchText)) 
+    }
+  }
 });
