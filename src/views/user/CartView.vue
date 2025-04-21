1785b1f3240adea993308c98ded5892e9dbef922
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-04-21 22:26:27 +0700

    Add "Shopping cart and  Order Summary" in Cart page

--- src/views/user/CartView.vue
+++ src/views/user/CartView.vue
@@ -1,11 +1,65 @@
 <script setup>
 import UserLayout from "@/layouts/UserLayout.vue";
-
+import close from "@/components/icon/close.vue";
 </script>
 
 <template>
-    <UserLayout>
-        Cart Page
-    </UserLayout>
-    
+  <UserLayout>
+    <h1 class="text-3xl font-bold m-4">Shopping Cart</h1>
+
+    <div class="flex">
+      <div class="flex-auto w-64 bg-base-200 p-4">
+        <div class="flex" v-for="itme in [1, 2, 3, 4, 5]">
+          <div class="flex-1">
+            <img class="w-full p-10" src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" />
+          </div>
+          <div class="flex-1">
+            <div class="flex flex-col justify-between h-full">
+              <div>
+                <div class="relative grid grid-cols-2">
+                  <div>
+                    <div><b>Flower</b></div>
+                    <div>Just Flower</div>
+                    <div>100 B</div>
+                  </div>
+                  <div>
+                    <select class="select w-1/2">
+                      <option v-for="quantity in [1, 2, 3, 4, 5]">{{ quantity }}</option>
+                    </select>
+                  </div>
+                  <div class="absolute top-0 right-0">
+                    <close></close>
+                  </div>
+                </div>
+              </div>
+              <div><b>In Stock</b></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex-auto w-32 bg-slate-200 p-4">
+        <div class="text-xl font-bold">Order Summary</div>
+        <div class=" my-4 divide-y">
+
+          <div class="flex justify-between py-2">
+            <div>ราคาสินค้าทั้งหมด</div>
+            <div>100</div>
+          </div>
+        
+          <div class="flex justify-between py-2">
+            <div>ค่าส่ง</div>
+            <div>0</div>
+          </div>
+        
+          <div class="flex justify-between py-2">
+            <div>ราคารวมทั้งหมด</div>
+            <div>100</div>
+          </div>
+        
+
+        </div>
+      </div>
+    </div>
+  </UserLayout>
 </template>
