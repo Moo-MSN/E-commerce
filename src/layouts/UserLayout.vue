79ddf6b3b4530069d6fc491e0e90be91bebe26cc
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-04-22 22:46:47 +0700

    Edit letters

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -66,12 +66,12 @@ const handleSearch = (event) =>{ // เพิ่ม search เพื่อให
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
-                <span class="badge badge-sm indicator-item">{{ cartStore.summaryQruantity }}</span>
+                <span class="badge badge-sm indicator-item">{{ cartStore.summaryQuantity }}</span>
               </div>
             </div>
             <div tabindex="0" class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
               <div class="card-body">
-                <span class="text-lg font-bold">{{ cartStore.summaryQruantity }} Items</span>
+                <span class="text-lg font-bold">{{ cartStore.summaryQuantity }} Items</span>
                 <span class="text-info">Subtotal: {{ cartStore.summaryPrice }} B</span>
                 <div class="card-actions">
                   <RouterLink :to="{ name: 'cart' }" class="btn btn-primary btn-block"> View cart </RouterLink>
