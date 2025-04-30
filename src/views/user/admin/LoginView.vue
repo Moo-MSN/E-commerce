fb04f8f9a5fa6590c94d8c6a076c95d8c29ec143
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-04-30 17:46:05 +0700

    import RouterLink and disign Login page

--- src/views/user/admin/LoginView.vue
+++ src/views/user/admin/LoginView.vue
@@ -1,9 +1,29 @@
 <script setup>
-import AdminLayout from "@/layouts/AdminLayout.vue";
+import { RouterLink } from 'vue-router';
+
+
 </script>
 
+
 <template>
-  <AdminLayout> 
-    AdminLogin page 
-    </AdminLayout>
+  <div class="h-screen flex items-center "> <!-- h-screen คือการตีเต็มหน้าจอ แล้วจัดให้อยู่ตรงกลางด้วย flex items-center-->
+    <div class="flex-1 max-w-2xl shadow-xl mx-auto p-8">
+      <div class="flex flex-col items-center">
+        <h1 class="text-3xl">Login</h1>
+        <div class="form-control w-full">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="text" placeholder="Your email here" class="input input-bordered w-full" />
+        </div>
+        <div class="form-control w-full">
+          <label class="label">
+            <span class="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="Your password" class="input input-bordered w-full" />
+        </div>
+        <RouterLink :to="{ name:'admin-dashboard' }" class="btn btn-neutral w-full mt-4">Login</RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
