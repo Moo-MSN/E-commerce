593fcbc947e9a8045f7b677b74fd7b4eb2280a44
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-04-30 15:48:33 +0700

    Add Router in All Admin page and insert components

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,24 @@ import ProfileView from "@/views/user/ProfileView.vue";
 import SearchView from "@/views/user/SearchView.vue";
 import SuccessView from "@/views/user/SuccessView.vue";
 
+//Admin Site
+import AdminLogin from "@/views/user/admin/LoginView.vue";
+import AdminDashboard from "@/views/user/admin/DashboardView.vue";
+//AdminProduct Site
+import AdminProductList from "@/views/user/admin/product/ListView.vue";
+import AdminProductUpdate from "@/views/user/admin/product/UpdateView.vue";
+//AdminUser Site
+import AdminUserList from "@/views/user/admin/user/ListView.vue";
+import AdminUserUpdate from "@/views/user/admin/user/UpdateView.vue";
+//AdminOrder Site
+import AdminOrderList from "@/views/user/admin/order/ListView.vue";
+import AdminOrderDetail from "@/views/user/admin/order/DetailView.vue";
+
+
+
+
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -38,6 +56,52 @@ const router = createRouter({
       path: "/success",
       name: "success",
       component: SuccessView,
+    },
+    //Admin Site
+    {
+      path: "/admin/login",
+      name: "admin-login",
+      component: AdminLogin,
+    },
+    {
+      path: "/admin/dashboard",
+      name: "admin-dashboard",
+      component: AdminDashboard,
+    },
+    {
+      path: "/admin/products",
+      name: "admin-product-list",
+      component: AdminProductList,
+    },
+    {
+      path: "/admin/products/create",
+      name: "admin-product-create",
+      component: AdminProductUpdate,
+    },
+    {
+      path: "/admin/products/update/:id",
+      name: "admin-product-update",
+      component: AdminProductUpdate,
+    },
+    {
+      path: "/admin/users",
+      name: "admin-users-list",
+      component: AdminUserList,
+    },
+    {
+      path: "/admin/users/update/:id",
+      name: "admin-users-update",
+      component: AdminUserUpdate,
+    },
+    {
+      path: "/admin/orders",
+      name: "admin-order-list",
+      component: AdminOrderList,
+    },
+    {
+      path: "/admin/orders/detail/:id",
+      name: "admin-orders-detail",
+      component: AdminOrderDetail,
     },
   ],
 });
---
+++ src/views/user/admin/product/ListView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AdminLayout from "@/layouts/AdminLayout.vue";
+</script>
+
+<template>
+  <AdminLayout> 
+    AdminProductList page 
+    </AdminLayout>
+</template>
