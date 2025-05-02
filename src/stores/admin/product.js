593cb9a6d46aa32e06a82cf1b3162e003e19e151
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-02 14:27:32 +0700

    create product.js for use in admin page

---
+++ src/stores/admin/product.js
@@ -0,0 +1,57 @@
+import { defineStore } from "pinia";
+
+export const useAdminProductStore = defineStore("product", {
+  state: () => ({
+    list: [
+      {
+        //เอาไว้เก็บ Array
+        name: "test",
+        image: "https://fastly.picsum.photos/id/531/200/300.jpg?hmac=zfPpbPSZ6_bXrCHp-HkvsObzLfkIb7pvmBoZhXrjKo4",
+        price: 200,
+        quantity: 20, // total quantity
+        remainQuantity: 11, // สร้างไปไว้ เพิ่มและลด จำนวนสินค้า
+        status: "open",
+        updatedAt: new Date().toISOString(), //จะได้ Date and Time ออกมา
+      },
+    ],
+  }),
+  actions: {
+    loadProduct() {
+      //สำหรับดึงข้อมูล product ทั้งหมด จาก localstorage และ save ใส่ state list
+      const productList = localStorage.getItem("product-data");
+      if (productList) {
+        this.list = JSON.parse(productList);
+      }
+    },
+    getProduct(index) {
+      //ดึงข้อมูล product จาก list
+      return this.list[index];
+    },
+    addProduct(productdata) {
+      //สำหรับ add ข้อมูล product เข้า list และ save ลง localstorage
+      productdata.remainQuantity = productdata.quantity; //remainQuantity กับ Quantity ตอนนี้เราจะให้เท่ากัน
+      productdata.updatedAt = new Date().toISOString();
+      this.list.push(productdata);
+      //save to localstorage
+      localStorage.setItem("product-data", JSON.stringify(this.list));
+    },
+    updateproduct(index, productdata) {
+      //สำหรับ update ข้อมูล product เข้า list และ save ลง localstorage
+      this.list[index].name = productdata.name;
+      this.list[index].image = productdata.image;
+      this.list[index].price = productdata.price;
+      this.list[index].quantity = productdata.quantity;
+      this.list[index].remainQuantity = productdata.remainQuantity;
+      this.list[index].status = productdata.status;
+      this.list[index].updatedAt = new Date().toISOString();
+      //save to localstorage
+      localStorage.setItem("product-data", JSON.stringify(this.list));
+    },
+    removeProduct(index) {
+      //สำหรับลบข้อมูล product จาก list และ save กลับ localstorage
+      this.list.splice(index, 1);
+      //save to localstorage
+      localStorage.setItem("product-data".JSON.stringify(this.list));
+    },
+  },
+});
