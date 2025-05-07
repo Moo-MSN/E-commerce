66b633f064e5cccde05fdecc94a09f36f9d6c97c
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-07 11:01:56 +0700

    import vue,vue-roter for recipe product-data to show in Edit page for update data

--- src/views/user/admin/product/UpdateView.vue
+++ src/views/user/admin/product/UpdateView.vue
@@ -1,7 +1,45 @@
 <script setup>
+import { reactive, onMounted, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
 import AdminLayout from "@/layouts/AdminLayout.vue";
+import { useAdminProductStore } from "@/stores/admin/product";
+
+const productIndex = ref(-1); // ประกาศตำแหน่ง productIndex
+const mode = ref("ADD"); // add mode เข้ามาเพื่อเวลาเปลี่ยนไปหน้า update จะได้เปลี่ยนจาก ADD to EDIT เมื่อมี route.params.id
+
+onMounted(() => {
+  if (route.params.id) {
+    // ถ้ามี route.params.id จะทำการเปลี่ยน mode จาก ADD เป็น EDIT
+    productIndex.value = parseInt(route.params.id);
+    mode.value = "EDIT";
+
+    const selectProduct = adminProductStore.getProduct(productIndex.value);
+    //เนื่องจาก reactive ไม่สามารถแทนทั้งตัวลงไปได้ เราเลยต้องทำแต่ละ field ออกมา
+    productData.name = selectProduct.name;
+    productData.image = selectProduct.image;
+    productData.price = selectProduct.price;
+    productData.quantity = selectProduct.quantity;
+    productData.about = selectProduct.about;
+    productData.status = selectProduct.status;
+  }
+});
+
+const adminProductStore = useAdminProductStore();
+const router = useRouter(); // เพิ่มเข้ามาเพื่อใช้ในการไปหน้าอื่น
+const route = useRoute();
+
+const productData = reactive({
+  //ใช้เก็บตัวค่า input
+  name: "",
+  image: "",
+  price: 0,
+  quantity: 0,
+  about: "",
+  status: "",
+});
 
 const formData = [
+  // ประกาศใช้ในการ loop ออกมาเป็น Form
   {
     name: "Name",
     field: "name",
@@ -23,17 +61,27 @@ const formData = [
     field: "about",
   },
 ];
+
+const updateproduct = () => {
+  if (mode.value === "EDIT") { // ถ้า mode.value เป็น EDIT ให้ทำการ update
+    adminProductStore.updateproduct(productIndex.value, productData);
+  } else {// แต่ถ้าไม่ใช่ EDIT ให้เพิ่ม product ใหม่
+    adminProductStore.addProduct(productData);
+  }
+  router.push({ name: "admin-products-list" }); // เมื่อ click addProduct จะเด้งไปหน้า products-list
+};
 </script>
 
 <template>
   <AdminLayout>
     <div class="shadow-xl p-8 mt-4">
-      <div class="text-3xl font-semibold">ADD</div>
+      <div class="text-3xl font-semibold">{{ mode }}</div>
       <div class="divider"></div>
       <div class="grid grid-cols-2 gap-4">
         <fieldset v-for="form in formData" class="fieldset">
           <legend class="fieldset-legend">{{ form.name }}</legend>
-          <input type="text" class="input" placeholder="Type here" />
+          <input v-model="productData[form.field]" type="text" class="input" />
+          <!--v-model เพื่อเก็บค่าในแต่ละ field ที่เราใส่ค่า-->
         </fieldset>
       </div>
 
@@ -41,7 +89,8 @@ const formData = [
       <div class="grid grid-cols-2 gap-4">
         <fieldset class="fieldset">
           <legend class="fieldset-legend">Status</legend>
-          <select class="select">
+          <select v-model="productData.status" class="select">
+            <!--v-model เพื่อเก็บค่าของ status-->
             <option disabled selected>Pick a Status</option>
             <option value="open">Open</option>
             <option value="close">Close</option>
@@ -49,8 +98,10 @@ const formData = [
         </fieldset>
       </div>
       <div class="flex justify-end mt-4">
-        <button class="btn btn-ghost">BACK</button>
-        <button class="btn btn-neutral">ADD</button>
+        <RouterLink :to="{ name: 'admin-dashboard' }" class="btn btn-ghost">BACK</RouterLink>
+        <!--เมื่อ click จะกลับยังหน้า dashboard-->
+        <button class="btn btn-neutral" @click="updateproduct()">{{ mode }}</button>
+        <!--เมื่อ click จะส่งค่าที่อยู่ใน reactive ไปใส่ใน productData-->
       </div>
     </div>
   </AdminLayout>
