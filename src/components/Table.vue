db4637f3a67bbfd97a2edbf7f4041ae38650b24a
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-07 14:08:07 +0700

    Create Table component for use another page

---
+++ src/components/Table.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  headers: Array,
+});
+</script>
+
+<template>
+  <!--สร้างขึ้นมาเพื่อใช้ในหน้าต่่างๆ ที่มีลักษณะที่ใช้ Table เหมือนๆกัน -->
+  <div class="overflow-x-auto">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th v-for="header in headers">{{ header }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <slot></slot>
+      </tbody>
+    </table>
+  </div>
+</template>
