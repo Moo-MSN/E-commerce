a4b8c42961ff6fdbc1078836bf432afc0b026bb5
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-07 14:10:04 +0700

    import Table for use new table by use form Table from components

--- src/views/user/admin/product/ListView.vue
+++ src/views/user/admin/product/ListView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import {onMounted } from "vue";
+import { onMounted } from "vue";
 import AdminLayout from "@/layouts/AdminLayout.vue";
 import { useAdminProductStore } from "@/stores/admin/product";
 import { RouterLink } from "vue-router";
 
+import Table from "@/components/Table.vue";
 import trash from "@/components/icon/trash.vue"; // import icon
 import edit from "@/components/icon/edit.vue"; // import icon
 
@@ -26,54 +27,35 @@ const removeProduct = (index) => {
         <RouterLink :to="{ name: 'admin-products-create' }" class="btn btn-neutral">Add New</RouterLink>
       </div>
     </div>
-    <div>
-      <div class="overflow-x-auto">
-        <table class="table">
-          <!-- head -->
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Image</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Status</th>
-              <th>Update At</th>
-              <th></th>
-              <!--สร้างไว้รอใส่ Action-->
-            </tr>
-          </thead>
-          <tbody>
-            <!-- row 1 -->
-            <tr v-for="(product, index) in adminProductStore.list">
-              <th>{{ product.name }}</th>
-              <td>
-                <img :src="product.image" class="w-12" />
-              </td>
-              <td>{{ product.price }}</td>
-              <td>{{ product.quantity }}</td>
-              <td>
-                <div class="badge gap-2" 
-                
-                :class="product.status === 'open' ?'badge-success': 'badge-error' "> 
-                <!--if else 1 บรรทัด คือ ถ้า open ให้เป็น badge-success ถ้าไม่ใช่ให้เป็น badge-error-->
-                  {{ product.status }}
-                </div> 
-              </td> 
-              <td>{{ product.updatedAt }}</td>
-              <td>
-                <div class="flex gap-2">
-                  <RouterLink :to="{name:'admin-products-update',params:{id:index}}" class="btn btn-ghost">
-                    <edit></edit>
-                  </RouterLink>
-                  <div class="btn btn-ghost" @click="removeProduct(index)"> <!--เมื่อ click จะทำการลบข้อมูลที่ตำแหน่ง index  -->
-                    <trash></trash>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+
+    <Table :headers="['Name', 'Image', 'Price', 'Quantity', 'Status', 'UpdateAt', '']">
+      <!-- row 1 -->
+      <tr v-for="(product, index) in adminProductStore.list">
+        <th>{{ product.name }}</th>
+        <td>
+          <img :src="product.image" class="w-12" />
+        </td>
+        <td>{{ product.price }}</td>
+        <td>{{ product.quantity }}</td>
+        <td>
+          <div class="badge gap-2" :class="product.status === 'open' ? 'badge-success' : 'badge-error'">
+            <!--if else 1 บรรทัด คือ ถ้า open ให้เป็น badge-success ถ้าไม่ใช่ให้เป็น badge-error-->
+            {{ product.status }}
+          </div>
+        </td>
+        <td>{{ product.updatedAt }}</td>
+        <td>
+          <div class="flex gap-2">
+            <RouterLink :to="{ name: 'admin-products-update', params: { id: index } }" class="btn btn-ghost">
+              <edit></edit>
+            </RouterLink>
+            <div class="btn btn-ghost" @click="removeProduct(index)">
+              <!--เมื่อ click จะทำการลบข้อมูลที่ตำแหน่ง index  -->
+              <trash></trash>
+            </div>
+          </div>
+        </td>
+      </tr>
+    </Table>
   </AdminLayout>
 </template>
