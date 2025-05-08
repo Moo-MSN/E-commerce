f59ed639c0163eb0e0c12514a061c715074d88dc
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-08 11:07:13 +0700

    Create user.js in store admin folder for use in Listview.vue in admin user

---
+++ src/stores/admin/user.js
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+
+export const useAdminUserStore = defineStore("admin-user", {
+  state: () => ({
+    list: [
+      {
+        adminName: "Moo",
+        role: "admin",
+        status: "active",
+        updatedAt: new Date().toISOString(),
+      },
+    ],
+  }),
+  actions: {
+    getUser(index) {
+      return this.list[index];
+    },
+    updateUser(index, userData) {
+      const fields = ["adminName", "role", "status"];
+      for (let field of fields) { // เป็นการ loop field ใน [] ออกมา
+        this.list[index][field] = userData[field];
+      }
+      this.list[index].updatedAt = userData.updatedAt;
+    }, // ตัวด้านบนสามารถเขียนอีกแบบได้เหมือนก้านล่าง
+    // ด้านล่างคือการใช้แบบยาว เก็บ index และค่าของ userData
+    //this.list[index].adminName = userData.adminName
+    //this.list[index].role = userData.role
+    //this.list[index].status = userData.status
+    //this.list[index].updatedAt = userData.updatedAt
+    removeUser(index) {
+        this.list.splice(index,1)
+    },
+  },
+});
