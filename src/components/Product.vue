0043d3d0f50f7dd10b321b13b99e1dbbec7aeaa0
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-14 10:45:32 +0700

    Edit imageURL to imageUrl

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -13,7 +13,7 @@ defineProps ({
       <div v-for="product in products " class="card bg-base-100 w-full shadow-sm">
         <!-- w-full ควรตีเต็มไว้จะได้จัดหน้าง่าย -->
         <figure>
-          <img class="w-full" :src="product.imageURL" />
+          <img class="w-full" :src="product.imageUrl" />
         </figure>
         <div class="card-body">
           <h2 class="card-title">{{ product.name }}</h2>
