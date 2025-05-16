4d77c9b5feabb550c512a212d9b2020eff83a5ca
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-16 15:31:23 +0700

    import Auth for use login in UserLayout

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,8 +22,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-connectFirestoreEmulator(db, "127.0.0.1",8080);
 
-export { db };
+const db = getFirestore(app);
+connectFirestoreEmulator(db, "127.0.0.1", 8080);
+
+const auth = getAuth(app);
+connectAuthEmulator(auth, "http://127.0.0.1:9099");
+
+export { db, auth };
