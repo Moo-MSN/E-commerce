a4e8c00410a680e8b66fb2a37c6b521b9e773f9c
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-17 10:54:37 +0700

    Add onAutustateChanged for hold login in a page

--- src/stores/account.js
+++ src/stores/account.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
 //import มาจาก UserLayoutเพื่อนำมาแยกให้ใช้ร่วมกันกับหน้าอื่นได้
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/firebase";
 
 const provider = new GoogleAuthProvider();
@@ -12,19 +12,32 @@ export const useAccountStore = defineStore("account", {
     user: {}, // เอาไว้เก็บ user information เอาไว้
   }),
   actions: {
+    async checkAuth() { // เพิ่มการ onAuthStateChanged เพื่อให้ login ค้างไว้ได้
+      onAuthStateChanged(auth, (user) => {
+        console.log("user", user);
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          this.user = user;
+          this.isLoggedIn = true;
+          const uid = user.uid;
+          // ...
+        }
+      });
+    },
     async signInWithGoogle() {
-        // try,catch เพื่อป้องกัน error
+      // try,catch เพื่อป้องกัน error
       try {
         const result = await signInWithPopup(auth, provider);
         this.isLoggedIn = true; //ถ้า login แล้วไม่มี error จะ login สำเร็จ
-        this.user = result.user; // ก็จะได้ user ที่ทำการ login ออกมา 
+        this.user = result.user; // ก็จะได้ user ที่ทำการ login ออกมา
+        console.log(this.user);
       } catch (error) {
         console.log("error", error);
       }
     },
-    async logout (){
-        this.isLoggedIn = false;
-        await signOut(auth)
-    }
+    async logout() {
+      this.isLoggedIn = false;
+      await signOut(auth);
+    },
   },
 });
