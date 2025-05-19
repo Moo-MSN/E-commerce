a95f1cd7b9e65c477269efb36fe11a9bb062be47
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-19 13:55:24 +0700

    Create signInAdmin in account.js for use in LoginView page

--- src/stores/account.js
+++ src/stores/account.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 
 //import มาจาก UserLayoutเพื่อนำมาแยกให้ใช้ร่วมกันกับหน้าอื่นได้
-import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase";
 
 const provider = new GoogleAuthProvider();
@@ -10,10 +10,11 @@ export const useAccountStore = defineStore("account", {
   state: () => ({
     isLoggedIn: false, // สร้างมาเช็คว่า login แล้วหรือยัง
     user: {}, // เอาไว้เก็บ user information เอาไว้
+    isAdmin: false, // เพิ่มมาสำหรับการ login แบบ Admin
   }),
   actions: {
     async checkAuth() {
-      // สร้าง new Promise เมื่อ user มีค่าให้ resolve เป็น true ถ้าไม่มีเป็น false 
+      // สร้าง new Promise เมื่อ user มีค่าให้ resolve เป็น true ถ้าไม่มีเป็น false
       return new Promise((resolve) => {
         // เพิ่มการ onAuthStateChanged เพื่อให้ login ค้างไว้ได้
         onAuthStateChanged(auth, (user) => {
@@ -40,8 +41,29 @@ export const useAccountStore = defineStore("account", {
         console.log("error", error);
       }
     },
+    // เพิ่มมาสำหรับ Admin Login เราสามารถสร้าง Admin user ได้ใน firebase authentication
+    async signInAdmin(email, password) {
+      try {
+        const result = await signInWithEmailAndPassword(auth, email, password);
+        this.isLoggedIn = true;
+        this.isAdmin = true;
+        this.user = result.user;
+      } catch (error) {
+        console.log("error store = ", error.code);
+        //การโยน error ไปแสดงใน console
+        switch (error.code) {
+          case 'auth/invalid-email':
+            throw new Error("Invalid-Email");
+          case 'auth/wrong-password':
+            throw new Error("Password incorrect")
+            default: // ถ้่าไม่เข้าเคส Invalid-Email จะแสดงด้านล่าง 
+              throw new Error("Something wrong in Login!!!")
+        }
+      }
+    },
     async logout() {
       this.isLoggedIn = false;
+      this.isAdmin = false;
       await signOut(auth);
     },
   },
--- src/views/user/admin/LoginView.vue
+++ src/views/user/admin/LoginView.vue
@@ -1,12 +1,32 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useAccountStore } from "@/stores/account"; // เพื่อใช้ signInAdmin
+import { useEventStore } from "@/stores/event"; // เพื่อใช้ Event ของ Toast
 
+const email = ref("");
+const password = ref("");
+const router = useRouter();
+const accountStore = useAccountStore();
+const evenStore = useEventStore();
 
+const Login = async () => {
+  console.log(email.value);
+  console.log(password.value);
+  try {
+    // ใส่ค่า email,password ของ user Admin ที่เราสร้างไว้ใน firebase
+    await accountStore.signInAdmin(email.value, password.value);
+    router.push({ name: "admin-dashboard" });
+  } catch (error) {
+    console.log("error page = ",error.message);
+    evenStore.popupMessage("error",error.message)
+  }
+};
 </script>
 
-
 <template>
-  <div class="h-screen flex items-center "> <!-- h-screen คือการตีเต็มหน้าจอ แล้วจัดให้อยู่ตรงกลางด้วย flex items-center-->
+  <div class="h-screen flex items-center">
+    <!-- h-screen คือการตีเต็มหน้าจอ แล้วจัดให้อยู่ตรงกลางด้วย flex items-center-->
     <div class="flex-1 max-w-2xl shadow-xl mx-auto p-8">
       <div class="flex flex-col items-center">
         <h1 class="text-3xl">Login</h1>
@@ -14,15 +34,15 @@ import { RouterLink } from 'vue-router';
           <label class="label">
             <span class="label-text">Email</span>
           </label>
-          <input type="text" placeholder="Your email here" class="input input-bordered w-full" />
+          <input v-model="email" type="text" placeholder="Your email here" class="input input-bordered w-full" />
         </div>
         <div class="form-control w-full">
           <label class="label">
             <span class="label-text">Password</span>
           </label>
-          <input type="password" placeholder="Your password" class="input input-bordered w-full" />
+          <input v-model="password" type="password" placeholder="Your password" class="input input-bordered w-full" />
         </div>
-        <RouterLink :to="{ name:'admin-dashboard' }" class="btn btn-neutral w-full mt-4">Login</RouterLink>
+        <button @click="Login()" class="btn btn-neutral w-full mt-4">Login</button>
       </div>
     </div>
   </div>
