b45e25a8c70fb75fa9a3c513fac86ac84d1638dc
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-05-27 21:56:43 +0700

    Add CRUD in project by connect to db firebase emulator

--- src/stores/admin/product.js
+++ src/stores/admin/product.js
@@ -1,4 +1,7 @@
 import { defineStore } from "pinia";
+// import firestore for use CRUD in product admin page
+import { collection, doc, getDoc, getDocs, setDoc, addDoc, deleteDoc } from "firebase/firestore";
+import { db } from "@/firebase";
 
 export const useAdminProductStore = defineStore("product-data", {
   state: () => ({
@@ -6,47 +9,77 @@ export const useAdminProductStore = defineStore("product-data", {
     loaded: false,
   }),
   actions: {
-    loadProduct() {
+    async loadProduct() {
+      // จิ้มไปที่ collection(db,"products")
+      const productCol = collection(db, "products");
+      // รับข้อมูลมาเก็บไว้ที่ productSnapshot
+      const productSnapshot = await getDocs(productCol);
+      // ทำการ convert productSnapshot.docs ด้วย .map แล้ว retrun เป็น doc.data() ที่ใช้ใน javaScript ได้ แล้วเก็บไว้ที่ products
+      const products = productSnapshot.docs.map(doc => {
+        const convertedProduct = doc.data();
+        // ทำการแนบ id ของ product ไปกับ convertedProduct ไปด้วยเพื่อใช้ในการระบุ id product
+        convertedProduct.productId = doc.id;
+        // ทำการแปลง time stamp ให้เป็น ว/ด/ป ที่ใช้ใน javaScript เพื่อให้เราอ่านง่าย
+        convertedProduct.updatedAt = convertedProduct.updatedAt.toDate();
+        return convertedProduct;
+      });
+
       //สำหรับดึงข้อมูล product ทั้งหมด จาก localstorage และ save ใส่ state list
-      const productList = localStorage.getItem("product-data");
-      if (productList) {
-        this.list = JSON.parse(productList);
-        this.loaded = true; // ถ้ามีการ loaded ไปแล้วจะเป็น ture
+      //const productList = localStorage.getItem("product-data");
+
+      this.list = products;
+    },
+    async getProduct(productId) {
+      try {
+        // จิ้มไปที่ doc(db, "products", productId)
+        const productRef = doc(db, "products", productId);
+        // รับ product ที่อยู่ใน products
+        const productSnapshot = await getDoc(productRef);
+        // ทำการ return แสดงข้อมูล
+        return productSnapshot.data();
+      } catch (error) {
+        console.log("error", error);
       }
     },
-    getProduct(index) {
-      //ดึงข้อมูล product จาก list
-      if (!this.loaded) {
-        // ถ้าไม่มีการ loaded
-        this.loadProduct(); // ให้ loadProduct ใหม่
+    async addProduct(productdata) {
+      try {
+        //สำหรับ add ข้อมูล product เข้า list
+        productdata.remainQuantity = productdata.quantity;
+        //remainQuantity กับ Quantity ตอนนี้เราจะให้เท่ากัน
+        productdata.updatedAt = new Date();
+        // จิ้มไปที่ collection(db,"products")
+        const productCol = collection(db, "products");
+        // ทำการ Add product โดยใช้ addDoc(productRef, productdata)
+        await addDoc(productCol, productdata);
+      } catch (error) {
+        console.log("error", error);
       }
-      return this.list[index];
     },
-    addProduct(productdata) {
-      //สำหรับ add ข้อมูล product เข้า list และ save ลง localstorage
-      productdata.remainQuantity = productdata.quantity; //remainQuantity กับ Quantity ตอนนี้เราจะให้เท่ากัน
-      productdata.updatedAt = new Date().toISOString();
-      this.list.push(productdata);
-      //save to localstorage
-      localStorage.setItem("product-data", JSON.stringify(this.list));
+    async updateProduct(productId, productdata) {
+      try {
+        const updateProduct = {};
+        updateProduct.name = productdata.name;
+        updateProduct.imageUrl = productdata.imageUrl;
+        updateProduct.price = productdata.price;
+        updateProduct.quantity = productdata.quantity;
+        updateProduct.remainQuantity = productdata.quantity; 
+        updateProduct.status = productdata.status;
+        updateProduct.updatedAt = new Date();
+        // จิ้มไปที่ doc(db,"products",productId)
+        const productRef = doc(db, "products", productId);
+        // ทำการ updateProduct โดยใช้ addDoc(productRef, productdata)
+        await setDoc(productRef, updateProduct);
+      } catch (error) {
+        console.log("error", error);
+      }
     },
-    updateProduct(index, productdata) {
-      //สำหรับ update ข้อมูล product เข้า list และ save ลง localstorage
-      this.list[index].name = productdata.name;
-      this.list[index].imageUrl = productdata.imageUrl;
-      this.list[index].price = productdata.price;
-      this.list[index].quantity = productdata.quantity;
-      this.list[index].remainQuantity = productdata.remainQuantity;
-      this.list[index].status = productdata.status;
-      this.list[index].updatedAt = new Date().toISOString();
-      //save to localstorage
-      localStorage.setItem("product-data", JSON.stringify(this.list));
-    },
-    removeProduct(index) {
-      //สำหรับลบข้อมูล product จาก list และ save กลับ localstorage
-      this.list.splice(index, 1);
-      //save to localstorage
-      localStorage.setItem("product-data", JSON.stringify(this.list));
+    async removeProduct(productId) {
+      try {
+        const productRef = doc(db, "products", productId);
+        await deleteDoc(productRef);
+      } catch (error) {
+        console.log("error", error);
+      }
     },
   },
 });
