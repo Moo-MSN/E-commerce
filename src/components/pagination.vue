f5f44bc28dc85b6b4332eebc9bd1bbff2ea8c62a
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-06-05 11:38:44 +0700

    Create pagination button in components folder

---
+++ src/components/pagination.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps ({
+  activePage: Number,
+  maxPage: Number,
+  changePage: Function
+})
+</script>
+
+<template>
+  <div class="join">
+    <button class="join-item btn" @click="changePage(activePage - 1)">«</button>
+    <button class="join-item btn">Page {{ activePage }} / {{ maxPage }}</button>
+    <button class="join-item btn" @click="changePage(activePage + 1)">»</button>
+  </div>
+</template>
