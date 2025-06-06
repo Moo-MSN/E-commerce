e7ae45602b411607cb6f24f33b0718e597838425
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-06-06 13:28:13 +0700

    Add v-if to button for show page number

--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps } from "vue";
 
-defineProps ({
+defineProps({
   activePage: Number,
   maxPage: Number,
-  changePage: Function
-})
+  changePage: Function,
+});
 </script>
 
 <template>
   <div class="join">
-    <button class="join-item btn" @click="changePage(activePage - 1)">«</button>
+    <button class="join-item btn" @click="changePage(activePage - 1)" v-if="activePage - 1 > 0">«</button>
     <button class="join-item btn">Page {{ activePage }} / {{ maxPage }}</button>
-    <button class="join-item btn" @click="changePage(activePage + 1)">»</button>
+    <button class="join-item btn" @click="changePage(activePage + 1)" v-if="activePage + 1 <= maxPage">»</button>
   </div>
 </template>
