ae9f716a087e947c4e4107ea569f47bdaa5ce792
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-06-09 10:08:04 +0700

    add comment for test remainQuantity

--- src/views/user/CheckoutView.vue
+++ src/views/user/CheckoutView.vue
@@ -37,7 +37,7 @@ const userFormData = reactive({
 
 const payment = () => {
   cartStore.placeorder(userFormData);
-  router.push({ name: "success" });
+  //router.push({ name: "success" });
 };
 </script>
 
