7036e2baa1c121311583d454ea8589077c0c8141
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-06-15 12:29:38 +0700

    add realtimeDB for use in cart store

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,8 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import {getStorage,connectStorageEmulator} from "firebase/storage"
 
+import {getDatabase, connectDatabaseEmulator} from "firebase/database"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -33,4 +35,7 @@ connectAuthEmulator(auth, "http://127.0.0.1:9099");
 const storage = getStorage()
 connectStorageEmulator(storage,"127.0.0.1", 9199)
 4000
-export { db, auth, storage };
+
+const realtimeDB = getDatabase()
+connectDatabaseEmulator(realtimeDB, "127.0.0.1", 9000)
+export { db, auth, storage, realtimeDB };
