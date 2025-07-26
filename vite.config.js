fb60bc335a9c8912f0f917c3184133b19290e40f
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-07-26 18:22:08 +0700

    Add server proxy for use API from Cloud functions

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,16 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  // เพิ่ม proxy เพื่อให้สามารถเรียก API จาก Firebase cloud Functions ได้
+   server: {
+    proxy: {
+      '/api': { // เพิ่มตรงนี้เข้ามา path ที่ต่อด้วย /api จะถูกแทนที่ด้วย target
+        target: 'http://127.0.0.1:5001/easy-commerce-by-moo-msn/us-central1/api',
+        changeOrigin: true, // และที่การ rewrithe เป็น target/ path ที่เราใช้งาน
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
