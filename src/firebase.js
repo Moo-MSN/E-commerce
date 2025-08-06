c12fe400df131a88154e0c11d875e78e8b0dce38
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-08-06 10:52:50 +0700

    edit port

--- src/firebase.js
+++ src/firebase.js
@@ -27,7 +27,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-connectFirestoreEmulator(db, "127.0.0.1", 8083);
+connectFirestoreEmulator(db, "127.0.0.1", 8084);
 
 const auth = getAuth(app);
 connectAuthEmulator(auth, "http://127.0.0.1:9099");
@@ -37,5 +37,5 @@ connectStorageEmulator(storage,"127.0.0.1", 9199)
 4000
 
 const realtimeDB = getDatabase()
-connectDatabaseEmulator(realtimeDB, "127.0.0.1", 9003)
+connectDatabaseEmulator(realtimeDB, "127.0.0.1", 9004)
 export { db, auth, storage, realtimeDB };
