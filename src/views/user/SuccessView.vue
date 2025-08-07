809a8f8e5cf9e77cc05036db5398f7d04ce0ea99
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-08-07 09:58:57 +0700

    import "useRoute" for get order detail and data by use "orderId in firebase" to success page

--- src/views/user/SuccessView.vue
+++ src/views/user/SuccessView.vue
@@ -1,17 +1,29 @@
 <script setup>
 import { ref, onMounted } from "vue";
+
+import { useRoute } from "vue-router"; // ใช้เพื่อเข้าถึง route ปัจจุบัน
+
 import UserLayout from "@/layouts/UserLayout.vue";
 
 import { useCartStore } from "@/stores/user/cart";
 
+const route = useRoute(); // เข้าถึง route ปัจจุบัน
+
 const cartStore = useCartStore();
 const orderData = ref({}); // ใช้ ref รับ {}
 
-onMounted(() => {
-  cartStore.loadCheckout();
-  if (cartStore.checkout.orderNumber) {
-    orderData.value = cartStore.checkout;
-  }
+onMounted(async () => {
+  // เรียกใช้ loadCheckout เพื่อโหลดข้อมูลการสั่งซื้อ
+  const orderId = route.query.order_id; // ดึง orderId จาก query params
+  if (orderId)
+    try {
+      orderData.value = await cartStore.loadCheckout(orderId); // เรียกใช้ loadCheckout จาก cartStore
+    } catch (error) {
+      console.error("Error loading checkout:", error);
+    }
+  //if (cartStore.checkout.orderNumber) {
+  //  orderData.value = cartStore.checkout;
+  //}
 });
 </script>
 
@@ -20,14 +32,14 @@ onMounted(() => {
     <div class="max-w-2xl mx-auto border border-base-200 shadow-xl p-8 my-4">
       <div>
         <div class="text-xl font-bold">Your order is succressful!</div>
-        <div>Hi {{ orderData.name }}</div>
+        <div>Hi : {{ orderData.name }}</div>
         <div>เตรียมรอรับสินค้าได้เลย</div>
       </div>
       <div class="divider"></div>
       <div class="grid grid-cols-4 gap-2">
         <div>
           <div class="font-bold">Order date</div>
-          <div>{{ orderData.createdData }}</div>
+          <div>{{ orderData.createdAt }}</div>
         </div>
         <div>
           <div class="font-bold">Order number</div>
@@ -35,7 +47,7 @@ onMounted(() => {
         </div>
         <div>
           <div class="font-bold">Payment method</div>
-          <div>{{ orderData.paymentMethod }}</div>
+          <div>{{ orderData.paymentMethod}}</div>
         </div>
         <div>
           <div class="font-bold">Address</div>
@@ -45,7 +57,7 @@ onMounted(() => {
       <div class="divider"></div>
       <div v-for="product in orderData.products" class="grid grid-cols-4 gap-2 mb-4 items-center">
         <div>
-          <img class="w-full" :src="product.imageURL" />
+          <img class="w-full" :src="product.imageUrl" />
         </div>
         <div><b>ชื่อสินค้า</b> : {{ product.name }}</div>
         <div>จำนวน : {{ product.quantity }}</div>
